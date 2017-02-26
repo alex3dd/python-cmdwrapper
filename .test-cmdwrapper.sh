fc0ec0789675fc6e1486fcf539d294412a5c622b
Asher256 <Asher256@users.noreply.github.com>
2017-02-25 22:38:28 -0500

    Travis CI

---
+++ .test-cmdwrapper.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#
+# test cmdwrapper
+#
+# Author: Achraf Cherti (aka Asher256) <asher256@gmail.com>
+# Website: http://www.asher256.com/
+# License: LGPL2.1
+#
+# This source code follows the Google style guide for shell scripts:
+# https://google.github.io/styleguide/shell.xml
+#
+
+set -o errexit
+set -o nounset
+
+main() {
+  local filename
+
+  for filename in cmdwrapper/*; do
+    echo "$filename"
+  done
+
+  exit 0
+}
+
+# MAIN
+main "$@"
+
+# vim:ai:et:sw=2:ts=2:sts=2:tw=0:fenc=utf-8
