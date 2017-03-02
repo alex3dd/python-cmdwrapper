64919cf3cd1759a33f61f9d3aaf4453ece7b23bc
Asher256 <Asher256@users.noreply.github.com>
2017-03-02 01:03:39 -0500

    fix

--- cmdwrapper/cmdproc.py
+++ cmdwrapper/cmdproc.py
@@ -238,7 +238,7 @@ def main():
             self.assertListEqual(cmd_list, ['ls', '/'])
             self.assertEqual(cmd_str, 'ls /')
 
-            self._cmd(cmd='pwd', cwd='/usr')
+            self._cmd(cmd='bash -c pwd', cwd='/usr')
             self.assertEqual(self._stdout, '/usr')
 
             self._cmd(cmd='bash -c "echo $MYTEST"',
